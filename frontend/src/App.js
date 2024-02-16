b9fb15dea5c7a5a4aba807ba1d410db82aefea7f
Brian Stasiukaitis <brianstas@gmail.com>
2024-02-15 23:03:23 -0500

    Updated app.js
    Fixed app.js to revert changes so the code is back to clean

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,43 +3,7 @@ import Layout from './layout/Layout';
 
 function App() {
   return (
-    <Router>
-      {/* <Navbar1 /> */}
-      <div className="app-container">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              isAuthenticated ? (
-                <Navigate to="/dashboard" replace />
-              ) : (
-                <HomePage />
-              )
-            }
-          />
-          {/* <Route path="/about" element={<AboutPage />} /> */}
-          <Route path="/login" element={<RegForm mode="login" />} />
-          <Route path="/register" element={<RegForm mode="register" />} />
-          <Route
-            path="/dashboard"
-            element={
-              isAuthenticated ? <Dashboard /> : <Navigate to="/" replace />
-            }
-          />
-          {/* <Route path="/profile" element={<Profile />} /> */}
-          {/* <Route path="/projects/:id" element={<ProjectDetail />} /> */}
-          {/* <Route
-            path="/projects/:projectId/lessons/:lessonId"
-            element={<ProjectLesson />}
-          /> */}
-          {/* <Route
-            path="/projects/:projectId/lessons/:lessonId/steps/:stepId"
-            element={<Step />}
-          /> */}
-        </Routes>
-      </div>
-      {/* <Footer /> */}
-    </Router>
+    <Layout />
   );
 }
 
