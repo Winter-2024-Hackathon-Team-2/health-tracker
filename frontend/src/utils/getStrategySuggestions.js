95155311ea2fbe3d6f763e457ec0b9a872fd810b
Devina Gillis <devinagillis0@gmail.com>
2024-02-19 12:38:14 -0800

    added file to handle calculations from survey to utils folder

---
+++ frontend/src/utils/getStrategySuggestions.js
@@ -0,0 +1,23 @@
+//Params: surveyForm
+/*
+TODO: 
+Get percentages of everything
+Return the strategyType with the min value
+Default to none if survey is incomplete
+*/
+
+function calculatePercentage(input, maxValue){
+    return input/()
+}
+
+export default function getStrategySuggestions(surveyInputs){
+    let strategyTypes = {
+        sleep: 0,
+        stress: 0,
+        "physical-activity": 0
+    }
+
+    for (let input of surveyInputs){
+        
+    }
+}
