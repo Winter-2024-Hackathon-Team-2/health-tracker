ccdd7795f4ced18ebf8b46026925eb1550b0c074
Devina Gillis <devinagillis0@gmail.com>
2024-02-22 07:35:54 -0800

    made heading more spaced out

--- frontend/src/strategies/ListStrategiesByType.js
+++ frontend/src/strategies/ListStrategiesByType.js
@@ -29,8 +29,8 @@ export default function ListStrategiesByType() {
     return (
       <>
       <section className="flex flex-col item-center text-center">
-      <h3 className="text-3xl">Moodscape Recommends</h3>
-      <p>Based on your survey, we've provided suggestions for {strategyType}.</p>
+      <h3 className="text-3xl pt-8 pb-3">Moodscape Recommends</h3>
+      <p className="pb-8">Based on your survey, we've provided suggestions for {strategyType}.</p>
         <StrategiesList strategies={strategies} />
         </section>
       </>
