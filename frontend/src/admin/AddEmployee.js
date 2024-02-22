d0b1a29615467ff87b4fabeafbce8699558d3ed2
Rouzbeh <vrouzbeh@yahoo.com>
2024-02-22 00:54:59 -0800

    Changed color of add user box

--- frontend/src/admin/AddEmployee.js
+++ frontend/src/admin/AddEmployee.js
@@ -48,7 +48,7 @@ function AddEmployee() {
       <div className="flex flex-1 items-center justify-center">
         <form
           onSubmit={handleSubmit}
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4"
         >
           <div className="mb-4">
             <label
