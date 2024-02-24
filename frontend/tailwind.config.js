1efd5de9c76827ca1740c701a9e0b01988b3d48d
Aaron <aaron.jazzar@gmail.com>
2024-02-24 17:37:44 -0500

    finishing touches

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-const default_theme = require('tailwindcss/defaultTheme')
+const default_theme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  use: ["source-map-loader"],
+
   theme: {
     extend: {
       colors: {
-        'dark-purple': '#393550',
-        'turqoise': '#00AADE',
-        'pale-yellow': '#F8EFA0',
-        'off-white': '#FAFAFA',
-        'almost-black': '#161626',
-        'custom-navy': '#063372',
-        'eggshell': '#E6F2FE',
-        'green-blue': '#088A9B',
+        "dark-purple": "#393550",
+        turqoise: "#00AADE",
+        "pale-yellow": "#F8EFA0",
+        "off-white": "#FAFAFA",
+        "almost-black": "#161626",
+        "custom-navy": "#063372",
+        eggshell: "#E6F2FE",
+        "green-blue": "#088A9B",
       },
       fontFamily: {
-        'sans': ['Poppins', ...default_theme.fontFamily.sans],
+        sans: ["Poppins", ...default_theme.fontFamily.sans],
       },
     },
   },
   plugins: [require("daisyui")],
-}
-
+};
